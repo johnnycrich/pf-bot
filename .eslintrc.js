16cd31609338ea8ef39fa6ae5e8b40f4fc289db8
Johnny Richardson <johnny.c.rich@gmail.com>
2020-06-01 23:49:07 -0400

    chore: add prettier, eslint, new deps, vscode workspace.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	env: {
+	  commonjs: true,
+	  es6: true,
+	  node: true,
+	},
+	extends: ['airbnb-base', 'eslint-config-prettier'],
+	globals: {
+	  Atomics: 'readonly',
+	  SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+	  ecmaVersion: 2018,
+	},
+	rules: {
+	  semi: ['error', 'always'],
+	  indent: ['error', 4],
+	  'arrow-parens': [2, 'as-needed'],
+	  'operator-linebreak': ['error', 'after'],
+	  'comma-dangle': ['error', {
+		objects: 'always'
+	  }],
+	},
+  };
